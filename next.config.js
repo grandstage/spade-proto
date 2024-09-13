d481791be5838b3e7cf641d3a1b8c2718730b447
peterr-spadeai <peterr@getspade.ai>
2024-09-13 16:18:19 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: 'build',
-    experimental: {
-        serverActions: true,
-    },
     images: {
       remotePatterns: [
         {
